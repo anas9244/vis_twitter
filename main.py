b8668fd940aa35982c64e228d7de310e95f5943c
Anas Alnayef <anas9244@hotmail.com>
2019-09-02 22:25:58 +0200

    finalize design

--- main.py
+++ main.py
@@ -11,47 +11,42 @@ import os
 
 fileDir = os.path.dirname(os.path.abspath(__file__)) + "/shape_files/"
 lod_changed = True
+
 shape_file = fileDir + 'ne_110m_admin_0_countries.shp'
 shape_file_ger = fileDir + "us_states.shp"
 datafile = 'obese.csv'
 
 
-gdf_world = gpd.read_file(shape_file)[['ADM0_A3', 'geometry']]
-gdf_ger = gpd.read_file(shape_file_ger)[['name', 'geometry']]
+gdf_world = gpd.read_file(shape_file)[['ISO_A2', 'ADMIN', 'TYPE', 'geometry']]
+
+gdf_ger = gpd.read_file(shape_file_ger)[['postal', 'name', 'type', 'geometry']]
 
 
-gdf_world.columns = ['country_code', 'geometry']
+gdf_world.columns = ['name', 'full_name', 'type', 'geometry']
 
-gdf_ger.columns = ['country_code', 'geometry']
+gdf_ger.columns = ['name', 'full_name', 'type', 'geometry']
 
 gdf_world = gdf_world.drop(gdf_world.index[159])
 
+dataframesList = [gdf_ger, gdf_world]
 
-# df = pd.read_csv(datafile, names=[
-#                  'entity', 'code', 'year', 'per_cent_obesity'], skiprows=1)
-
-
-# #yr = selectedYear
-# df_yr = df[df['year'] == 2016]
-# merged = gdf_world.merge(gdf_ger, left_on='country_code',
-#                          right_on='code', how='left')
-# # merged.fillna('No data', inplace=True)
-merged_json = json.loads(gdf_world.to_json())
-json_data = json.dumps(merged_json)
+rdf = gpd.GeoDataFrame(pd.concat(dataframesList, ignore_index=True))
+rdf['count'] = 0
+rdf.loc[rdf['name'] == 'DC', 'type'] = 'State'
 
 
-# def json_data(selectedYear):
-#     yr = selectedYear
-#     df_yr = df[df['year'] == yr]
-#     merged = gdf.merge(df_yr, left_on='country_code',
-#                        right_on='code', how='left')
-#     # merged.fillna('No data', inplace=True)
-#     merged_json = json.loads(merged.to_json())
-#     json_data = json.dumps(merged_json)
-#     return json_data
+country_rdf = rdf[rdf['type'] != 'State']
+state_rdf = rdf[rdf['type'] == 'State']
 
 
-geosource = GeoJSONDataSource(geojson=json_data)
+country_json = json.loads(country_rdf.to_json())
+country_data = json.dumps(country_json)
+
+state_json = json.loads(state_rdf.to_json())
+state_data = json.dumps(state_json)
+
+
+geosource = GeoJSONDataSource(geojson=country_data)
 
 
 palette = brewer['YlGnBu'][8]
@@ -59,10 +54,10 @@ palette = brewer['YlGnBu'][8]
 palette = palette[::-1]
 
 color_mapper = LinearColorMapper(
-    palette=palette, low=0, high=40)
+    palette=palette, low=0, high=1000)
 
-tick_labels = {'0': '0%', '5': '5%', '10': '10%', '15': '15%',
-               '20': '20%', '25': '25%', '30': '30%', '35': '35%', '40': '>40%'}
+tick_labels = {'0': '0', '125': '125', '250': '250', '375': '375',
+               '500': '500', '625': '625', '750': '750', '875': '875', '1000': '>1000'}
 
 color_bar = ColorBar(background_fill_color="#222222", color_mapper=color_mapper, label_standoff=8, width=600, height=20,
                      border_line_color=None, location='center', orientation='horizontal', major_label_overrides=tick_labels, major_label_text_color="white")
@@ -78,47 +73,37 @@ p.background_fill_color = "#2a2a2a"
 
 
 cr = p.patches('xs', 'ys', source=geosource,
-               line_color='#dedede', line_width=0.5, fill_alpha=1, hover_fill_color="pink", hover_line_color="pink")
+               line_color='#dedede', line_width=0.5, hover_fill_color="pink", hover_line_color="pink", fill_color={'field': 'count', 'transform': color_mapper})
 
 p.add_tools(HoverTool(tooltips=[
-    ("Country", "@country_code"), ("per_cent_obesity", "@per_cent_obesity")], renderers=[cr]))
+    ("Name", "@full_name"), ("Num. of Tweets", "@count")], renderers=[cr]))
 p.add_layout(color_bar, 'below')
 
 
 # p.lod_factor = 10000
 
 
-def update_title(attr, new, old):
+def update_lod(attr, new, old):
 
     global lod_changed
-    global gdf_world
+    #global gdf_world
 
     if new and old:
         if abs(p.x_range.start - p.x_range.end) < 140 and lod_changed:
 
             print("detail")
-            gdf_world_remove_cont = gdf_world.drop(gdf_world.index[121])
-            gdf_world_remove_cont = gdf_world_remove_cont.drop(
-                gdf_world_remove_cont.index[4])
-            dataframesList = [gdf_ger, gdf_world_remove_cont]
 
-            rdf = gpd.GeoDataFrame(
-                pd.concat(dataframesList, ignore_index=True))
-            merged_json = json.loads(rdf.to_json())
-            json_data = json.dumps(merged_json)
-            geosource.geojson = json_data
-
+            geosource.geojson = state_data
             lod_changed = False
 
         elif abs(p.x_range.start - p.x_range.end) > 140 and not lod_changed:
             print("all")
-            merged_json = json.loads(gdf_world.to_json())
-            json_data = json.dumps(merged_json)
-            geosource.geojson = json_data
+
+            geosource.geojson = country_data
             lod_changed = True
 
 
-p.x_range.on_change('end', update_title)
+p.x_range.on_change('end', update_lod)
 layout = column(p)
 
 
