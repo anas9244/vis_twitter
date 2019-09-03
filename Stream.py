01815997760ede8f611a566d1ca7fbb7dcb1bb8d
Anas Alnayef <anas9244@hotmail.com>
2019-09-04 01:19:52 +0200

    flashing

--- Stream.py
+++ Stream.py
@@ -1,86 +1,93 @@
 import json
-import os.path
+
 import tweepy
-import glob
+
+
+import pickle
 import os
 
-import datetime
-import main
+# access_token = "588509788-1z07dMNOMhOCw4OyLJwxSfse31TR7Aywj6h2uZgd"
+# access_token_secret = "WPlTT3cpVXREXWuGvjdbxD8ie92e61vadWFlzxcoesjHe"
+# consumer_key = "Em7YjncUOkyjxzZhP3hWWUDJL"
+# consumer_secret = "IXkMkxVh1eFq9FJpo5vjI1NsTlzAscsEezVRjxhHZIBAnJiaEO"
 
-
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_token = "1118134186274062339-MGNkDPMhhQn0uwZlVY73T1tFvJvJ8w"
+access_token_secret = "D6eXlgVJiJ5q3RQBAO03GhizanMVItD8dIZOhsBvujhUO"
+consumer_key = "WC98mcJLSHMNna64T0EyzhGhV"
+consumer_secret = "UKp18iWGxIZxcriW0nl1MD9b12R8fP7TIEUNb83eX1M91Vbzq9"
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+fileDir = os.path.dirname(os.path.abspath(__file__))
+
+place_tweets_file = open(fileDir + "/place_tweets.pickle", "rb")
+place_tweets = pickle.load(place_tweets_file)
+place_tweets_file.close()
+
+
+# file_out = open("tweet.json", "w")
 
 
 class TwitterListener(tweepy.StreamListener):
 
     def __init__(self):
         super(TwitterListener, self).__init__()
-        self.num_tweets = 0
-        self.valid = False
-
-        self.file_index = 0
-        self.file = set_file(self.file_index)
 
     def on_data(self, raw_data):
         try:
-            json_data = json.loads(raw_data)
-            if valid_chk(json_data):
-                if self.num_tweets >= 36347:
-                    return False
-                self.num_tweets += 1
+            tweet = json.loads(raw_data)
 
-                # Write to new file every 100K tweet
-                if self.num_tweets % 100000 == 0:
-                    currentDT = datetime.datetime.now()
-                    log_stuff("number of tweets: " +
-                              str(self.num_tweets) + ", Time: " + str(currentDT))
+            if not tweet['text'].startswith('RT') and tweet['place']:
+                country_code = tweet['place']['country_code']
+                if country_code == 'US' and tweet['place']['place_type'] == "city":
+                    state = tweet['place']['full_name'].split(', ')[1]
 
-                    # Write last tweet out of 100K to the current file; otherwise it would be 99999
-                    json.dump(json_data, self.file, ensure_ascii=False)
+                    place_tweets[state] += 1
 
-                    # Close current file
-                    self.file.close()
-                    # increment filename for next 100K tweets
-                    self.file_index += 1
-                    self.file = set_file(self.file_index)
-                    print(self.num_tweets)
+                place_tweets[country_code + '_c'] += 1
+                # print(tweet['text'])
 
-                # elif self.num_tweets <= 1000000:
+                return ("yes")
+                # file_out.seek(0)
+                # file_out.truncate()
+                # json.dump(place_tweets, file_out)
 
-                else:
-                    json.dump(json_data, self.file, ensure_ascii=False)
-                    self.file.write("\n")
-                    print(raw_data)
-
-                # else:
-                #     print("Finished!", self.num_tweets)
+                # def stop_streaming():
+                #     print("disconnect")
                 #     return False
-                return True
+
+            # return True
         except Exception as ex:
-            log_stuff("Unhandled exception from on_status: " + str(ex))
+
             print(ex)
             return True
 
     def on_error(self, status_code):
         if status_code == 420:
             print(" rate limit is reached")
-            with open("log.txt", 'w') as file:
-                file.write("rate limit is reached")
+
             return False
         else:
-            log_stuff("status code error: " + str(status_code))
-
             print("Error: ", status_code)
             return True
 
     def on_exception(self, exception):
         print("Error:", exception)
 
-        log_stuff("on_exception error: " + str(exception))
+
+listener = TwitterListener()
+twitterStream = tweepy.Stream(auth, listener)
+
+
+def start_streaming(query):
+    print("start")
+    global place_tweets
+    place_tweets = {place: 0 for place in place_tweets}
+    twitterStream.filter(track=[query], is_async=True)
+
+
+def get_counts():
+
+    global place_tweets
+    return (place_tweets)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from bokeh.palettes import brewer
 from bokeh.layouts import widgetbox, column, row
 from bokeh.models.widgets import Button, TextInput
 import os
+import pickle
 import Stream
 
 
@@ -16,14 +17,14 @@ fileDir = os.path.dirname(os.path.abspath(__file__)) + "/shape_files/"
 lod_changed = True
 
 shape_file = fileDir + 'ne_110m_admin_0_countries.shp'
-shape_file_ger = fileDir + "us_states.shp"
+shape_file_states = fileDir + "us_states.shp"
 datafile = 'obese.csv'
 
 
 gdf_country = gpd.read_file(
     shape_file)[['ISO_A2', 'ADMIN', 'TYPE', 'geometry']]
 
-gdf_state = gpd.read_file(shape_file_ger)[
+gdf_state = gpd.read_file(shape_file_states)[
     ['postal', 'name', 'type', 'geometry']]
 
 
@@ -36,15 +37,14 @@ dataframesList = [gdf_state, gdf_country]
 
 rdf = gpd.GeoDataFrame(pd.concat(dataframesList, ignore_index=True))
 rdf['count'] = 0
+rdf['flash'] = 0
 rdf.loc[rdf['name'] == 'DC', 'type'] = 'State'
+
+rdf.loc[rdf['type'] != 'State', 'name'] += '_c'
 
 
 country_rdf = rdf[rdf['type'] != 'State']
 state_rdf = rdf[rdf['type'] == 'State']
-
-
-country_set = set(country_rdf['name'])
-state_set = set(state_rdf['name'])
 
 country_data = json.dumps(json.loads(country_rdf.to_json()))
 state_data = json.dumps(json.loads(state_rdf.to_json()))
@@ -53,22 +53,30 @@ state_data = json.dumps(json.loads(state_rdf.to_json()))
 geosource = GeoJSONDataSource(geojson=country_data)
 
 
-palette = brewer['YlGnBu'][8]
+# palette = brewer['YlGnBu'][8]
 
-palette = palette[::-1]
+# palette = palette[::-1]
 
+
+palette = ['#293238', '#245472', '#3684b1', '#4e9bc9',
+           '#78b3d6', '#aacfe6', '#d7e8f2', '#ffffff']
 color_mapper = LinearColorMapper(
-    palette=palette, low=0, high=1000)
+    palette=palette, low=0, high=8)
 
-tick_labels = {'0': '0', '125': '125', '250': '250', '375': '375',
-               '500': '500', '625': '625', '750': '750', '875': '875', '1000': '>1000'}
 
-color_bar = ColorBar(background_fill_color="#222222", color_mapper=color_mapper, label_standoff=8, width=600, height=20,
+palette_flash = ['#254a60', '#ffffff']
+color_mapper_flash = LinearColorMapper(
+    palette=palette_flash, low=0, high=1)
+
+tick_labels = {'0': '0', '1': '1', '2': '2', '3': '3',
+               '4': '4', '5': '5', '6': '6', '7': '7', '8': '>8'}
+
+color_bar = ColorBar(background_fill_color="#222222", color_mapper=color_mapper, label_standoff=5, width=600, height=20,
                      border_line_color=None, location='center', orientation='horizontal', major_label_overrides=tick_labels, major_label_text_color="white")
 
 
 p = figure(tooltips=None,
-           width=1500, height=720, tools="pan,wheel_zoom", active_scroll="wheel_zoom", title_location="above",)
+           width=1500, height=720, tools="pan,wheel_zoom", active_scroll="wheel_zoom", title_location="above")
 
 p.grid.visible = False
 p.toolbar.logo = None
@@ -79,49 +87,138 @@ p.background_fill_color = "#2a2a2a"
 
 
 cr = p.patches('xs', 'ys', source=geosource,
-               line_color='#dedede', line_width=0.5, hover_fill_color="pink", hover_line_color="pink", fill_color={'field': 'count', 'transform': color_mapper})
+               line_color={'field': 'flash', 'transform': color_mapper_flash}, line_width=0.9, hover_fill_color={'field': 'count', 'transform': color_mapper}, hover_line_color="white", fill_color={'field': 'count', 'transform': color_mapper})
+
 
 p.add_tools(HoverTool(tooltips=[
     ("Name", "@full_name"), ("Num. of Tweets", "@count")], renderers=[cr]))
+p.hover.point_policy = "follow_mouse"
 p.add_layout(color_bar, 'below')
-
-
-# p.lod_factor = 10000
 
 
 def update_lod(attr, new, old):
 
     global lod_changed
-    #global gdf_world
+    # global gdf_world
 
     if new and old:
-        if abs(p.x_range.start - p.x_range.end) < 140 and lod_changed:
+        if abs(p.x_range.start - p.x_range.end) < 120 and lod_changed:
 
             print("detail")
 
+            color_mapper = LinearColorMapper(
+                palette=palette, low=0, high=8)
+
+            tick_labels = {'0': '0', '1': '1', '2': '2', '3': '3',
+                           '4': '4', '5': '5', '6': '6', '7': '7', '8': '>8'}
+
+            color_bar.color_mapper = color_mapper
+            color_bar.major_label_overrides = tick_labels
+
+            # state_rdf = rdf[rdf['type'] == 'State']
+            rdf_us = rdf.drop(rdf.index[55])
+
+            state_data = json.dumps(json.loads(rdf_us.to_json()))
             geosource.geojson = state_data
             lod_changed = False
 
-        elif abs(p.x_range.start - p.x_range.end) > 140 and not lod_changed:
+        elif abs(p.x_range.start - p.x_range.end) > 130 and not lod_changed:
             print("all")
+
+            color_mapper = LinearColorMapper(
+                palette=palette, low=0, high=8)
+
+            tick_labels = {'0': '0', '1': '1', '2': '2', '3': '3',
+                           '4': '4', '5': '5', '6': '6', '7': '7', '8': '>8'}
+            color_bar.color_mapper = color_mapper
+            color_bar.major_label_overrides = tick_labels
+
+            country_rdf = rdf[rdf['type'] != 'State']
+
+            country_data = json.dumps(json.loads(country_rdf.to_json()))
 
             geosource.geojson = country_data
             lod_changed = True
 
 
-def test_click():
-    print('Start')
+straming = False
+
+
+def start_button_click():
+
+    # print("start")
+    curdoc().add_periodic_callback(update_counts, 1000)
+    Stream.start_streaming(text_input.value)
+    global straming
+    straming = True
+
+    rdf['count'] = 0
+    if lod_changed:
+
+        country_rdf = rdf[rdf['type'] != 'State']
+
+        country_data = json.dumps(json.loads(country_rdf.to_json()))
+
+        geosource.geojson = country_data
+    else:
+        rdf_us = rdf.drop(rdf.index[55])
+
+        state_data = json.dumps(json.loads(rdf_us.to_json()))
+        geosource.geojson = state_data
+
+
+def stop_button_click():
+
+    print("stop")
+    Stream.twitterStream.disconnect()
+    global straming
+    straming = False
+    rdf['flash'] = 0
+
+
+# def update_count():
+    # start_button_click
 
 
 p.x_range.on_change('end', update_lod)
 start_button = Button(label="Start", button_type="primary", width=100)
-start_button.on_click(test_click)
+start_button.on_click(start_button_click)
 stop_button = Button(label="Stop", button_type="danger", width=100)
-stop_button.on_click(test_click)
+stop_button.on_click(stop_button_click)
 text_input = TextInput(value="default", width=600)
 layout = column(row(text_input, start_button, stop_button), p)
 
 
+def update_counts():
+
+    if straming:
+
+        new_dict = Stream.get_counts()
+        for index, place in enumerate(new_dict):
+            if rdf.iloc[index]['count'] != new_dict[place]:
+                rdf.loc[rdf['name'] == place, 'count'] = new_dict[place]
+                rdf.loc[rdf['name'] == place, 'flash'] = 1
+
+        if lod_changed:
+
+            country_rdf = rdf[rdf['type'] != 'State']
+
+            country_data = json.dumps(json.loads(country_rdf.to_json()))
+
+            geosource.geojson = country_data
+
+        else:
+            rdf_us = rdf.drop(rdf.index[55])
+
+            state_data = json.dumps(json.loads(rdf_us.to_json()))
+            geosource.geojson = state_data
+
+
+def flash():
+    rdf['flash'] = 0
+
+
+curdoc().add_periodic_callback(flash, 200)
 curdoc().add_root(layout)
 
 show(p)
